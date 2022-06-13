066be402b031d2a3f5bd30b0f3603a28391d3ea1
MAR2807 <105993122+MAR2807@users.noreply.github.com>
2022-06-13 17:41:03 -0400

    update 4

--- src/App.js
+++ src/App.js
@@ -32,21 +32,21 @@ function App() {
                 <Route  exact path = "/Main"  element={ <Main />}>
                  
                 </Route>
-                {/* <Route exact path = "/SignIn" element={auth.currentUser? <Navigate to = "/Main" /> : <Navigate to="/SignIn"/>}> */}
+               
                 <Route exact path = "/SignIn" element={ <SignIn/>}> 
 
-                {/* <Route exact path = "/SignIn" element={<SignIn />}>  */}
+              
                 </Route>
                 <Route exact path = "/SignUp" element={<SignUp />}>
                   
                 </Route>
 
                 <Route exact path = "/authenticate" element={auth.currentUser ? <Navigate to ="/Main" /> : <SignIn/> }>
-                {/* <Navigate to="/SignIn"/> */}
+               
                   
                 </Route>
                 <Route exact path = "/authenticate2" element={auth.currentUser ? <Main /> : <SignUp/> }>
-                {/* <Navigate to="/SignIn"/> */}
+              
                   
                 </Route>
               </Routes>
